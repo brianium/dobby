7b3ddf84440bd80f6ba8139e461ab3c7ac5e8d5f
Brian Scaturro <scaturrob@gmail.com>
2023-09-01 15:34:48 -0400

    fix: add hook for defagent macro

--- resources/clj-kondo.exports/dobby/dobby/clj_kondo/dobby.clj
+++ resources/clj-kondo.exports/dobby/dobby/clj_kondo/dobby.clj
@@ -1,11 +1,43 @@
 (ns clj-kondo.dobby
   (:require [clj-kondo.hooks-api :as api]))
 
+(defn defagent-hook
+  "Transforms a defunction into a defn"
+  [{:keys [node]}]
+  (let [[name _ _ binding-vec & body] (rest (:children node))
+        [sym val]                        (:children binding-vec)]
+    (when-not (and sym val)
+      (throw (ex-info "No sym and val provided" {})))
+    (let [new-node (api/list-node
+                    (list
+                     (api/token-node 'def)
+                     name
+                     (api/list-node
+                      (list
+                       (api/token-node 'fn)
+                       (api/list-node
+                        (list
+                         (api/vector-node [_ _ _])
+                         (api/list-node
+                          (list*
+                           (api/token-node 'fn)
+                           (api/vector-node [sym val])
+                           body))))
+                       (api/list-node
+                        (list
+                         (api/vector-node [_ _])
+                         (api/list-node
+                          (list*
+                           (api/token-node 'fn)
+                           (api/vector-node [sym val])
+                           body))))))))] 
+      {:node new-node})))
+
 (defn defunction-hook
   "Transforms a defunction into a defn"
   [{:keys [node]}]
   (let [[name docstring _ binding-vec & body] (rest (:children node))
-        [sym val]                                  (:children binding-vec)]
+        [sym val]                             (:children binding-vec)]
     (when-not (and sym val)
       (throw (ex-info "No sym and val provided" {})))
     (let [new-node (api/list-node
