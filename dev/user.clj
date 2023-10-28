363e0a4905eeafd929b0d3c722f79f52082ffe2e
Brian Scaturro <scaturrob@gmail.com>
2023-10-28 16:46:20 -0400

    agent as a seq too

--- dev/user.clj
+++ dev/user.clj
@@ -1,10 +1,8 @@
 (ns user
   (:require [clojure.core.async :as async :refer [<! go-loop]]
             [clojure.tools.namespace.repl :as repl]
-            [dobby.impl.bidi :as bidi]
-            [dobby.gpt :as gpt]
-            [juxt.clip.repl :refer [start stop reset set-init! system]])
-  (:refer-clojure :exclude [agent]))
+            [dobby.impl.agent :as agent]
+            [juxt.clip.repl :refer [start stop reset set-init! system]]))
 
 (repl/set-refresh-dirs "src" "dev")
 
@@ -25,47 +23,16 @@
                :messages [{:role    "user"
                            :content "Hello, world!"}]})
 
-(defn agent
-  ([log input output]
-   (let [*log         (atom log)
-         size         (count (.buf output))
-         
-         mult         (async/mult output)
-         read-stream  (async/chan (when (pos? size) size)) ;;; A channel for reading output externally
-         write-stream (async/chan (when (pos? size) size)) ;;; A channel for reading internally for updating the log
-         
-         bidi         (bidi/bidi-chan input read-stream)]
-     (async/tap mult read-stream)
-     (async/tap mult write-stream)
-     (go-loop []
-       (when-some [msg (<! input)]
-         (let [log    (swap! *log conj msg)
-               stream (gpt/stream {:model    "gpt-3.5-turbo"
-                                   :messages log})]
-           (async/pipeline-async 1 output (fn [val ch]
-                                            (async/put! ch val)
-                                            (async/close! ch)) stream)
-           (recur))))
-     (go-loop []
-       (when-some [response (<! write-stream)]
-         (let [[id body] response]
-           (when (= id :response/completed)
-             (swap! *log conj body)))
-         (recur)))
-     (assoc bidi :log *log)))
-  ([log input]
-   (agent log input (async/chan)))
-  ([log]
-   (agent log (async/chan) (async/chan))))
-
-(def c (agent [{:role "system" :content "You are an acient Sicilian from the 8th century B.C"}] (async/chan) (async/chan)))
+(def c (agent/create-agent [{:role "system" :content "You are an ancient Sicilian from the 8th century B.C"}] (async/chan) (async/chan)))
 
 (go-loop []
   (when-some [msg (<! c)]
     (println msg)
     (recur)))
 
-#_(put! c {:role "user" :content "Tell me a little about yourself"})
+#_(put! c {:role "user" :content "Hello there!"})
+
+(map :content c)
 
 #_@(:log c)
 
--- src/dobby/impl/agent.clj
+++ src/dobby/impl/agent.clj
@@ -1,10 +1,31 @@
 (ns dobby.impl.agent
   (:require [clojure.core.async :as async :refer [<! go-loop]]
+            [clojure.core.async.impl.protocols :as async.proto]
             [dobby.impl.bidi :as bidi]
             [dobby.impl.gpt :as gpt])
   (:refer-clojure :exclude [agent]))
 
-(defn agent
+(deftype Agent [bidi log]
+  async.proto/ReadPort
+  (take! [_ handler]
+    (async.proto/take! bidi handler))
+  
+  async.proto/WritePort
+  (put! [_ val handler] 
+    (async.proto/put! bidi val handler))
+  
+  async.proto/Channel
+  (close! [_]
+    (async.proto/close! bidi))
+  
+  clojure.lang.Seqable
+  (seq [_]
+    (seq @log)))
+
+(defn create-agent
+  "Creates an agent. An agent is a bidirectional channel that can be used to send and receive messages
+   from gpt. It functions as a core.async channel that can be read from and written to. It maintains a stateful
+   log"
   ([log input output]
    (let [*log         (atom log)
          size         (count (.buf output))
@@ -13,9 +34,12 @@
          read-stream  (async/chan (when (pos? size) size)) ;;; A channel for reading output externally
          write-stream (async/chan (when (pos? size) size)) ;;; A channel for reading internally for updating the log
 
-         bidi         (bidi/bidi-chan input read-stream)]
+         agent        (Agent. (bidi/bidi-chan input read-stream) *log)]
+     ;;; All output goes to the read and write streams
      (async/tap mult read-stream)
      (async/tap mult write-stream)
+
+     ;;; Handles input sent to agent - responses are piped to the mult'ed output channel
      (go-loop []
        (when-some [msg (<! input)]
          (let [log    (swap! *log conj msg)
@@ -25,14 +49,18 @@
                                             (async/put! ch val)
                                             (async/close! ch)) stream)
            (recur))))
+     
+     ;;; Reads exclusively from the write-stream in order to update the log with response data
      (go-loop []
        (when-some [response (<! write-stream)]
          (let [[id body] response]
            (when (= id :response/completed)
              (swap! *log conj body)))
          (recur)))
-     (assoc bidi :log *log)))
+     
+     ;;; Return the agent
+     agent))
   ([log input]
-   (agent log input (async/chan)))
+   (create-agent log input (async/chan)))
   ([log]
-   (agent log (async/chan) (async/chan))))
+   (create-agent log (async/chan) (async/chan))))
