869ef3e39acfb7b38a08005fa53ad9780d2b5bf6
Brian Scaturro <scaturrob@gmail.com>
2023-10-29 19:15:16 -0400

    grumbos and flurbos

--- dev/user.clj
+++ dev/user.clj
@@ -9,16 +9,41 @@
   [agent str]
   (put! agent {:role "user" :content str}))
 
-(def swordsman (dobby/agent (io/resource "swordsman.txt")))
+(defn reply!
+  "Takes an assistant response and treats it as a reply to the given agent - effectively
+   converting the assistant role to a user role"
+  [agent response]
+  (let [[type data] response]
+    (when (= type :response/completed)
+      (say! agent (:content data)))))
 
-;;; Let's read from the swordsman as output becomes available
+(defn dialog!
+  "Prints a reply out with a label to help visualize a conversation"
+  [agent label response]
+  (when (reply! agent response)
+    (println (format "\n%s: %s" label (:content (second response))))))
+
+(def grumbos-fan (dobby/agent (io/resource "grumbos-fan.txt")))
+
+(def flurbos-fan (dobby/agent (io/resource "flurbos-fan.txt")))
+
 (go-loop []
-  (when-some [msg (<! swordsman)]
-    (println msg)
+  (when-some [response (<! grumbos-fan)] 
+    (dialog! flurbos-fan "Grumbos Fan" response)
     (recur)))
 
-#_(say! swordsman "What is your name?")
+(go-loop []
+  (when-some [response (<! flurbos-fan)]
+    (dialog! grumbos-fan "Flurbos Fan" response)
+    (recur)))
 
-#_(map :content swordsman)
+#_(say! grumbos-fan "Well, well, welll... If it isn't a no good Grumbos fan!")
 
-#_(close! swordsman)
+#_(->> (filter #(= (:role %) "assistant") grumbos-fan)
+       (map :content))
+
+#_(->> (filter #(= (:role %) "assistant") flurbos-fan)
+       (map :content))
+
+#_(doseq [ag [grumbos-fan flurbos-fan]]
+    (close! ag))
